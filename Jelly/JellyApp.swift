a6efb4b0747089169217cc50966cedbb0f779ff7
Jian Ma <jianquanma@gmail.com>
2025-06-01 18:42:07 -0400

    Initial Commit

---
+++ Jelly/JellyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JellyApp.swift
+//  Jelly
+//
+//  Created by Jian Ma on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct JellyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JellyTests/JellyTests.swift
@@ -0,0 +1,17 @@
+//
+//  JellyTests.swift
+//  JellyTests
+//
+//  Created by Jian Ma on 6/1/25.
+//
+
+import Testing
+@testable import Jelly
+
+struct JellyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
